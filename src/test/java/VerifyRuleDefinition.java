1b58f6557233c2a112c7d400efe67be538251756
remote_access <81193887+syndit@users.noreply.github.com>
2025-06-29 20:04:11 -0400

    Version 3 work in progress source backup

--- src/test/java/VerifyRuleDefinition.java
+++ src/test/java/VerifyRuleDefinition.java
@@ -9,8 +9,6 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-package test.java;
-
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -66,9 +64,9 @@ public class VerifyRuleDefinition {
 
 		eval.evaluateRule(12);
 		
-		System.out.println(eval.getPassNumberOutcome(12, "fee"));
-		System.out.println(eval.getPassTagOutcome(1, "i18n"));
-		System.out.println(eval.getPassTagOutcome(11, "i18n"));
+		System.out.println(eval.getNumberOutcome(12, "fee"));
+		System.out.println(eval.getTagOutcome(1, "i18n"));
+		System.out.println(eval.getTagOutcome(11, "i18n"));
 		
 		System.out.println(eval.getVariables());
 		
