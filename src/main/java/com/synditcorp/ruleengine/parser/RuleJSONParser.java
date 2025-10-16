9789d2feba985f80103010551c474b913a1f0491
remote_access <81193887+syndit@users.noreply.github.com>
2025-10-16 12:26:20 -0400

    Fixing type in class documentation

--- src/main/java/com/synditcorp/ruleengine/parser/RuleJSONParser.java
+++ src/main/java/com/synditcorp/ruleengine/parser/RuleJSONParser.java
@@ -32,8 +32,8 @@ public class RuleJSONParser implements RuleParser {
 	 * Loads the JSON definition file.  Pass a String with the path and file name, or a java.nio.file.Path object.  This parser uses the Jackson parser. 
 	 * If getting definitions from resources like MongoDB, create a new parser that implements the RuleParser interface.
 	 * 
-	 * @param agruments java.nio.file.Path object
-	 * @param agruments java.lang.String
+	 * @param arguments java.nio.file.Path object
+	 * @param arguments java.lang.String
 	 * @throws DefinitionResourceException
 	 */
 	@Override
